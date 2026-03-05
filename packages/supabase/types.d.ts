e6a14f3d06c74ac46e2e0ba421a6699aa4fc33bb
streetwalker111777-star <streetwalker111777@gmail.com>
2026-03-05 18:58:14 +0100

    Add Profile type and ProfileStatus to types.d.ts

---
+++ packages/supabase/types.d.ts
@@ -0,0 +1,10 @@
+export type ProfileStatus = 'pending' | 'approved' | 'rejected'
+
+export interface Profile {
+  id: string
+  updated_at: string | null
+  username: string | null
+  full_name: string | null
+  avatar_url: string | null
+  website: string | null
+}
